f1c72a7c2be6f5bf66e640ad0c57c9276e05b367
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:26:03 +0700

    Introduces `MataKuliah` views

--- mata_kuliah/views.py
+++ mata_kuliah/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from mata_kuliah.serializers import MataKuliahSerializer
+from mata_kuliah.models import MataKuliah
+
+
+class MataKuliahListView(ListAPIView):
+    serializer_class = MataKuliahSerializer
+    queryset = MataKuliah.objects.all().order_by('id')
