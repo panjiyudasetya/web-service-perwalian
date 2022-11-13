2708e45c8526d93dbcc647262bf07a0f9d83ae74
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:41:26 +0700

    Introduces `Perwalian` Admin Page

--- perwalian/admin.py
+++ perwalian/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from perwalian.models import Perwalian
+
+
+class PerwalianAdmin(admin.ModelAdmin):
+    list_display = (
+        'kode', 'mahasiswa', 'dosen', 'mulai_tahun_ajaran',
+        'akhir_tahun_ajaran', 'tanggal_perwalian', 'masalah', 'saran',
+    )
+    list_per_page = 25
+    raw_id_fields = ('mahasiswa', 'dosen',)
+
+
+admin.site.register(Perwalian, PerwalianAdmin)
