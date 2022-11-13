b531aa7dccd612196af999ce8527959dffedcc0b
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:25:43 +0700

    Introduces `MataKuliah` Admin Page

--- mata_kuliah/admin.py
+++ mata_kuliah/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from mata_kuliah.models import MataKuliah
+
+
+class MataKuliahAdmin(admin.ModelAdmin):
+    list_display = (
+        'kode', 'dosen', 'nama', 'sks',
+    )
+    list_per_page = 25
+    raw_id_fields = ('dosen',)
+
+
+admin.site.register(MataKuliah, MataKuliahAdmin)
