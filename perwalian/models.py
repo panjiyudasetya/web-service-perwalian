260aabef8f72f22d7a257da30e2a90a0cd38a30a
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:41:15 +0700

    Introduces `Perwalian` models

--- perwalian/models.py
+++ perwalian/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from dosen.models import Dosen
+from mahasiswa.models import Mahasiswa
+
+
+class Perwalian(models.Model):
+    kode = models.CharField(max_length=12)
+    mahasiswa = models.ForeignKey(
+        Mahasiswa,
+        on_delete=models.CASCADE,
+        related_name='perwalian_as_mhs',
+    )
+    dosen = models.ForeignKey(
+        Dosen,
+        on_delete=models.CASCADE,
+        related_name='perwalian_as_dosen',
+    )
+    mulai_tahun_ajaran = models.PositiveIntegerField()
+    akhir_tahun_ajaran = models.PositiveIntegerField()
+    tanggal_perwalian = models.DateField()
+    masalah = models.TextField(null=True, blank=True)
+    saran = models.TextField(null=True, blank=True)
