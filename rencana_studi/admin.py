d4c1e87f2f1d7e9b4383b707ab72cfc747830db2
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:56:28 +0700

    Introduces `RencanaStudi` Admin Page

--- rencana_studi/admin.py
+++ rencana_studi/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from rencana_studi.models import RencanaStudi
+
+
+class RencanaStudiAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'perwalian', 'mata_kuliah', 'grade'
+    )
+    list_per_page = 25
+    raw_id_fields = ('perwalian', 'mata_kuliah',)
+
+
+admin.site.register(RencanaStudi, RencanaStudiAdmin)
