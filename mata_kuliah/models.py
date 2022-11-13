c5c5e3fc683556164c20610dc7e3a69429828f2c
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:25:17 +0700

    Introduces `MataKuliah` models

--- mata_kuliah/models.py
+++ mata_kuliah/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from dosen.models import Dosen
+
+
+class MataKuliah(models.Model):
+    kode = models.CharField(max_length=8)
+    dosen = models.ForeignKey(
+        Dosen,
+        on_delete=models.CASCADE
+    )
+    nama = models.CharField(max_length=152)
+    sks = models.PositiveIntegerField()
