b6861672835df0fd6c2e304b4e963e2d19670129
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:56:18 +0700

    Introduces `RencanaStudi` models

--- rencana_studi/models.py
+++ rencana_studi/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from mata_kuliah.models import MataKuliah
+from perwalian.models import Perwalian
+
+
+class RencanaStudi(models.Model):
+    perwalian = models.ForeignKey(
+        Perwalian,
+        on_delete=models.CASCADE
+    )
+    mata_kuliah = models.ForeignKey(
+        MataKuliah,
+        on_delete=models.CASCADE
+    )
+    grade = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True
+    )
