b65f74af4021622874a59c995383e6be12366da6
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-13 15:12:18 +0700

    Introduces `Dosen` Admin Page

--- dosen/admin.py
+++ dosen/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from dosen.models import Dosen
+
+
+class DosenAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'nid', 'nama_depan', 'nama_belakang', 'alamat',
+    )
+    list_per_page = 25
+
+
+admin.site.register(Dosen, DosenAdmin)
